2b23f10a5197bf9bd78d73f27d097b65ca959907
kenshien <kenshienworkacc@gmail.com>
2026-01-11 19:43:24 +0800

    added a funtion of the hamburger menu and exit, almost done in the navigation

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -1,4 +1,7 @@
-import { Leaf, ShoppingCart } from "lucide-react";
+"use client";
+
+import { Leaf, Menu, ShoppingCart, X } from "lucide-react";
+import { useState } from "react";
 
 const linkInfo = [
   {
@@ -29,24 +32,26 @@ const linkInfo = [
 ];
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <nav className="border-olive/10 sticky top-0 z-50 border-b shadow-sm backdrop-blur-sm">
-      <div className="container mx-auto flex items-center justify-between md:justify-around">
+      <div className="container mx-auto flex items-center justify-between lg:justify-around">
         {/* logo */}
         <div className="flex items-center gap-2 p-5">
-          <div className="bg-sage flex h-10 w-10 items-center justify-center rounded-full">
-            <Leaf className="w-2xl" color="white" />
+          <div className="bg-sage flex h-8 w-8 items-center justify-center rounded-full sm:h-9 sm:w-9 lg:h-10 lg:w-10">
+            <Leaf className="size-5 sm:size-6 lg:size-7" color="white" />
           </div>
-          <span className="text-olive font-heading text-2xl">
+          <span className="text-olive font-heading text-[12px] sm:text-[18px] lg:text-2xl">
             GreenRoot Co.
           </span>
         </div>
 
-        {/* links */}
+        {/* links (deskstop) */}
         <div className="hidden gap-2 md:flex">
           {linkInfo.map((items) => (
             <div
-              className="p-2 text-[18px] lg:p-4 lg:text-[20px] xl:p-6 xl:text-2xl"
+              className="p-2 text-[18px] md:text-[15px] lg:p-4 lg:text-[20px] xl:p-6 xl:text-2xl"
               key={items.id}
             >
               <a
@@ -59,15 +64,36 @@ export default function Navbar() {
           ))}
         </div>
 
+        {/* links (mobile) */}
+        <div className={`sidebar gap-2 ${isOpen ? "flex" : "hidden"}`}>
+          <button className="p-4" onClick={() => setIsOpen(false)}>
+            <X className="text-charcoal size-10" />
+          </button>
+          {linkInfo.map((items) => (
+            <div
+              className="hover:bg-olive flex w-full justify-center p-5 text-[18px] md:text-[15px] lg:p-4 lg:text-[20px] xl:p-6 xl:text-2xl"
+              key={items.id}
+            >
+              <a className="text-charcoal font-body" href={items.path}>
+                {items.label}
+              </a>
+            </div>
+          ))}
+        </div>
+
         {/* Shop */}
-        <div className="hover:bg-warm-beige/50 fit-w flex items-center justify-center rounded-full p-5 transition-all">
-          <ShoppingCart className="text-olive" />
+        <div className="flex items-center justify-center gap-2 p-5">
+          <button className="hover:bg-warm-beige/50 flex rounded-full p-2 transition-all">
+            <ShoppingCart className="text-olive size-5 sm:size-8" />
+          </button>
+          <button
+            className="hover:bg-warm-beige/50 flex rounded-full p-2 transition-all md:hidden"
+            onClick={() => setIsOpen(true)}
+          >
+            <Menu className="text-olive size-5 sm:size-8" />
+          </button>
         </div>
       </div>
     </nav>
   );
 }
-
-
-// responsiveness 
-// hamburger menu need todo 
